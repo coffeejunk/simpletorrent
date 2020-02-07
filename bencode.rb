bd076dc33b8ccc7ceb41087153b7311b6c07e60b
Maximilian Haack <max@haack.xyz>
2020-02-07 19:23:48 +0100

    Use StringIO instead of StringScanner
    - Deal with UTF8 char issues

--- bencode.rb
+++ bencode.rb
@@ -1,42 +1,74 @@
-require 'strscan'
+require "stringio"
 
 class Bencode
-  def Bencode.decode_scaner s
-    case control = s.getch
-    when "i"
-      # Integers go between start and end markers, so 7 would encode to i7e.
-      num = s.scan(/-?\d+/).to_i
-      s.skip "e"
-      num
-    when /\d+/
-      # Strings come with a length prefix, and look like 4:spam.
-      chars = control.to_i
-      s.skip ":"
-      str = s.scan /.{#{chars}}/
-    when "l"
-      # Lists: l4:spami7ee represents ['spam', 7],
-      ary = []
-
-      while s.peek(1) != "e" do
-        ary << decode_scaner(s)
-      end
-
-      ary
-    when "d"
-      # Hash/Dict: d4:spami7ee means {spam: 7}.
-      hsh = {}
-
-      while s.peek(1) != "e" do
-        key = decode_scaner(s)
-        value = decode_scaner(s)
-        hsh[key] = value
-      end
-
-      hsh
+  def self.decode string
+    if string.is_a? IO
+      load(string)
+    else
+      load(StringIO.new(string))
     end
   end
 
-  def Bencode.decode string
-    decode_scaner(StringScanner.new(string))
+  def self.load s
+    case peek(s)
+    when "i"
+      decode_int s
+    when "0".."9"
+      Bencode.decode_string s
+    when "l"
+      Bencode.decode_array s
+    when "d"
+      Bencode.decode_dict s
+    end
+  end
+
+  # Integers go between start and end markers, so 7 would encode to i7e.
+  def self.decode_int s
+    s.getc
+
+    s.gets("e").chop.to_i
+  end
+
+  # Strings come with a length prefix, and look like 4:spam.
+  def self.decode_string s
+    # get length of string
+    num = s.gets(":").chop.to_i
+
+    # get actual string
+    s.read(num.to_i).force_encoding Encoding.default_external
+  end
+
+  # Lists: l4:spami7ee represents ['spam', 7],
+  def self.decode_array s
+    s.getc
+    ary = []
+
+    while peek(s) != "e"
+      ary << load(s)
+    end
+
+    s.getc
+
+    ary
+  end
+
+  # Hash/Dict: d4:spami7ee means {spam: 7}.
+  def self.decode_dict s
+    s.getc
+    hsh = {}
+
+    while peek(s) != "e"
+      (key = load(s)) || raise("DecodeError: No key for dict")
+      value = load(s)
+      hsh[key] = value
+    end
+
+    hsh
+  end
+
+  def self.peek s
+    c = s.getc
+    s.ungetc c
+    c
   end
 end
--- test/bencode_test.rb
+++ test/bencode_test.rb
@@ -1,6 +1,6 @@
 # this was taken from https://github.com/dasch/ruby-bencode
-require 'minitest/autorun'
-require 'shoulda'
+require "minitest/autorun"
+require "shoulda"
 
 require_relative "../bencode"
 
@@ -15,19 +15,31 @@ end
 describe "decoding" do
   it_should_decode 42, "i42e"
   it_should_decode 0, "i0e"
-  it_should_decode -42, "i-42e"
+  it_should_decode(-42, "i-42e")
 
   it_should_decode "foo", "3:foo"
   it_should_decode "", "0:"
 
   it_should_decode [1, 2, 3], "li1ei2ei3ee"
 
+  it_should_decode ["array", ["one", "two", "three"]],
+    "l5:arrayl3:one3:two5:threeee"
+
   hsh = {"foo" => "bar", "baz" => "qux"}
   it_should_decode hsh, "d3:foo3:bar3:baz3:quxe"
 
-  # it_should_decode 42, "i42eBOGUS", :ignore_trailing_junk => true
+  hsh_2 = {"array" => ["one", "two", "three"], "integer" => 42, "string" => "bar"}
+  it_should_decode hsh_2, "d5:arrayl3:one3:two5:threee7:integeri42e6:string3:bare"
 
-  # for now stick to ASCII
-  # it_should_decode "café", "5:café"
-  # it_should_decode ["你好", "中文"], "l6:你好6:中文e"
+  # # it_should_decode 42, "i42eBOGUS", :ignore_trailing_junk => true
+
+  it_should_decode "café", "5:café"
+  it_should_decode ["你好", "中文"], "l6:你好6:中文e"
+end
+
+describe "bittorrent" do
+  it "should load a bencoded torrent file" do
+    Bencode.decode(File.open("debian-10.2.0-amd64-netinst.iso.torrent"))
+    pass
+  end
 end
